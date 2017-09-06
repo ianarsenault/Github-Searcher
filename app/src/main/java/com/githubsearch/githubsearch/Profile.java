5332ebe688432763b9701c04a78594dd3a3cd790
Ian Arsenault <ian_arsenault@protonmail.com>
2017-09-06 16:21:53 -0400

    added static key variables

--- app/src/main/java/com/githubsearch/githubsearch/Profile.java
+++ app/src/main/java/com/githubsearch/githubsearch/Profile.java
@@ -19,6 +19,53 @@ public class Profile extends AppCompatActivity {
 
     private TextView varDumpDisplay;
 
+    private static final String KEY_AVATAR = "avatar_url";
+    private static final String KEY_LOGIN = "login";
+    private static final String KEY_LOCATION = "location";
+    private static final String KEY_BLOG = "blog";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_FOLLOWING = "following";
+    private static final String KEY_FOLLOWERS = "followers";
+    private static final String KEY_NUM_OF_REPOS = "public_repos";
+    private static final String KEY_SITE_URL = "url";
+    private static final String KEY_STARRED_URL = "starred_url";
+    private static final String KEY_REPOS_URL = "repos_url";
+    private static final String KEY_MEMBER_SINCE = "created_at";
+
+    /**
+    {"received_events_url":"https://api.github.com\/users\/GorgonsMaze\/received_events",
+            "organizations_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/orgs",
+            "avatar_url":"https:\/\/avatars3.githubusercontent.com\/u\/12011826?v=4",
+            "gravatar_id":"",
+            "public_gists":5,
+            "location":"localhost",
+            "site_admin":false,
+            "blog":"https:\/\/keybase.io\/ian_a",
+            "type":"User",
+            "id":12011826,
+            "following":19,
+            "followers":11,
+            "public_repos":34,
+            "name":"Ian Arsenault",
+            "following_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/following{\/other_user}",
+            "created_at":"2015-04-18T20:54:01Z",
+            "events_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/events{\/privacy}",
+            "login":"GorgonsMaze",
+            "subscriptions_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/subscriptions",
+            "repos_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/repos",
+            "gists_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/gists{\/gist_id}",
+            "starred_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/starred{\/owner}{\/repo}",
+            "url":"https:\/\/api.github.com\/users\/GorgonsMaze",
+            "html_url":"https:\/\/github.com\/GorgonsMaze",
+            "hireable":true,
+            "updated_at":"2017-08-24T04:20:07Z",
+            "bio":null,
+            "email":null,
+            "company":null,
+            "followers_url":"https:\/\/api.github.com\/users\/GorgonsMaze\/followers"}
+
+     **/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +85,12 @@ public class Profile extends AppCompatActivity {
 
         try {
             JSONObject jObj = new JSONObject(jsonString);
-            Log.d("My App",jObj.toString());
+            Log.d("My App: ",jObj.toString());
             // Display unordered JSON
             varDumpDisplay.setText(jObj.toString().replace("\\",""));
 
         } catch (JSONException e) {
-            Log.e("MYAPP", "unexpected JSON Exception", e);
+            Log.e("My App: ", "unexpected JSON Exception", e);
         }
 
 
