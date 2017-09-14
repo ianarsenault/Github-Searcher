638e19d1a104b288c417430472aa2a59e78050b2
Ian Arsenault <ian_arsenault@protonmail.com>
2017-09-14 16:13:53 -0400

    wip parsing repo data to listviews

--- app/src/main/java/com/githubsearch/githubsearch/ReposActivity.java
+++ app/src/main/java/com/githubsearch/githubsearch/ReposActivity.java
@@ -10,8 +10,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.githubsearch.githubsearch.R.id.textViewTest;
 
 public class ReposActivity extends AppCompatActivity {
+
+    private ImageView profilePicture;
+    private TextView username;
+    private TextView test;
+    private JSONArray repoArray;
+    private JSONObject json_repo;
+
+    private static final String KEY_NAME = "name";
+    private static final String KEY_FULLNAME = "full_name";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_REPO_URL = "url";
+    private static final String KEY_LAST_UPDATE = "updated_at";
+    private static final String KEY_LANGUAGE = "language";
+    private static final String KEY_FORKS_COUNT = "forks_count";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +49,55 @@ public class ReposActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.github_icon);
         setSupportActionBar(toolbar);
+
+
+        profilePicture = (ImageView) findViewById(R.id.imageViewProfilePicture);
+        username = (TextView) findViewById(R.id.textViewUsername);
+        test = (TextView) findViewById(R.id.textViewTest);
+
+        Intent intent = getIntent();
+        String profileImageUrl = intent.getStringExtra("profileimage");
+        String loginId = intent.getStringExtra("userid");
+        String profileRepoArrayString = intent.getStringExtra("reposarray");
+
+        try {
+            repoArray = new JSONArray(profileRepoArrayString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Using Picasso lib to load profile image
+        Picasso.with(this)
+                .load(profileImageUrl)
+                .error(R.drawable.profilepicture)
+                .into(profilePicture);
+
+        username.setText("@" + loginId);
+
+        ListView repoResults = (ListView) findViewById(R.id.listViewRepo);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+
+        for (int i = 0; i < repoArray.length(); i++) {
+            try {
+                json_repo = repoArray.getJSONObject(i);
+                json_repo.getString(KEY_FULLNAME)
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // resolve json gets to strings or ints to display in set text
+            test.setText(json_repo.getString(KEY_FULLNAME));
+        }
+
+
+//        Toast toast = Toast.makeText(this, String.valueOf(repoArray.length()), Toast.LENGTH_LONG);
+//        toast.show();
+
+
+
 
 
     }
